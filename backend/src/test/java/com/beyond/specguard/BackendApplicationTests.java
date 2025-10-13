dc83af048fa7feac43e35a1ec5c60e48fafb7fba
viroovr <tjgusdnjs789@gmail.com>
2025-10-13 12:42:41 +0900

    [Feat] 통합테스트 코드 제거

--- backend/src/test/java/com/beyond/specguard/BackendApplicationTests.java
+++ backend/src/test/java/com/beyond/specguard/BackendApplicationTests.java
@@ -1,13 +1,15 @@
-package com.beyond.specguard;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+// package com.beyond.specguard;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.ActiveProfiles;
+//
+// @SpringBootTest
+// @ActiveProfiles("test")
+// class BackendApplicationTests {
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
